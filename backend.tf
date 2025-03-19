cbf3307a8683d83c95160481329b831fd574309d
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-18 19:18:57 -0500

    add backend add provider add variables

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "~> 1.5"
+  backend "s3" {
+    bucket  = "data-architecture-terraform-state-bucket"
+    region  = "us-east-1"
+    key     = "s3-github-actions/terraform.tfstate"
+    encrypt = true
+  }
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  description = "The name of the bucket"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region to use."
+  type        = string
+  default     = "us-east-1"
+}
