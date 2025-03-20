175eeaf4a2c1d1fc2b36ac307e3e8f37d16880a1
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-20 13:54:04 -0500

    update data lambda function logic to use kinesis

--- mocks/mock_data_lambda_function.py
+++ mocks/mock_data_lambda_function.py
@@ -1,9 +1,18 @@
+import boto3
 import json
-import random
+
+# Initialize a Kinesis client with the boto3 library
+kinesis_client = boto3.client('kinesis')
 
 def lambda_handler(event, context):
-    temperature = random.uniform(20.0, 30.0)  # Random temperature between 20 and 30
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'temperature': temperature})
-    } 
+    temperature = ... # Your code to generate the temperature reading
+    data = json.dumps({'temperature': temperature})
+    
+    # Send the data to Kinesis
+    response = kinesis_client.put_record(
+        StreamName='TemperatureDataStream',
+        Data=data,
+        PartitionKey='partition_key'  # This can be any string like 'temp'
+    )
+    
+    return response 
