85653ffb942683eebc8583c5ef6097b2b348ddba
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-19 23:48:07 -0500

    add tf lockfile true

--- backend.tf
+++ backend.tf
@@ -1,11 +1,10 @@
 terraform {
   required_version = "~> 1.5"
   backend "s3" {
-    bucket  = "data-architecture-terraform-state-bucket"
-    region  = "us-east-1"
-    key     = "PROD/s3-github-actions/terraform.tfstate"
-    encrypt = true
-    # tflint-ignore: all
-    use_lockfile = false
+    bucket       = "data-architecture-terraform-state-bucket"
+    region       = "us-east-1"
+    key          = "PROD/s3-github-actions/terraform.tfstate"
+    encrypt      = true
+    use_lockfile = true #S3 native locking
   }
 }
