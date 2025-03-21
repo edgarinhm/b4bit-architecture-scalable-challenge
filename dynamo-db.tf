ab1a6220fcd4b800d8788b023f08fd37bdab7349
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-21 18:11:53 -0500

    add dynamodb add vpc add variables

---
+++ dynamo-db.tf
@@ -0,0 +1,32 @@
+
+resource "aws_dynamodb_table" "data_lake" {
+  name         = var.aws_dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = var.aws_dynamodb_table_name
+  }
+}
+
+# Data source to dynamically fetch the service name for DynamoDB
+data "aws_vpc_endpoint_service" "dynamodb" {
+  service      = "dynamodb"
+  service_type = "Gateway"
+}
+
+# VPC Endpoint for DynamoDB
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id            = aws_vpc.vpc.id
+  service_name      = data.aws_vpc_endpoint_service.dynamodb.service_name
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = [
+    aws_route_table.private_route_table_az1.id,
+    aws_route_table.private_route_table_az2.id
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,23 @@
+# environment variables
+variable "aws_account_id" {
+  description = "AWS account id"
+  type        = string
+}
+
 variable "aws_region" {
   description = "The AWS region to use."
   type        = string
   default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "environment"
+  type        = string
 }
 
 variable "aws_python_version" {
@@ -15,4 +31,36 @@ variable "aws_s3_bucket_data_lake_name" {
   type        = string
   default     = "data-aggregation-lake"
 
+}
+variable "aws_dynamodb_table_name" {
+  description = "The name of the DynamoDB table."
+  type        = string
+  default     = "data-table-aggregation-lake"
+
+}
+
+# vpc variables
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type        = string
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "public subnet az1 cidr block"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "public subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  description = "private app subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  description = "private app subnet az2 cidr block"
+  type        = string
 }
