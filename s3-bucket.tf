55616be1b127356abbb1e1740989a1b3c189a5fe
Edgar Mejia <edgarinhm@hotmail.com>
2025-03-20 19:47:34 -0500

    add kinesis role policy fix s3 log bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -5,38 +5,6 @@ resource "aws_s3_bucket" "data_lake" {
   }
 }
 
-resource "aws_s3_bucket_acl" "data_lake_acl" {
-  bucket = aws_s3_bucket.data_lake.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket_encryption" {
-  bucket = aws_s3_bucket.log_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = "arn"
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
-}
-
-resource "aws_s3_bucket_logging" "data_lake_logging" {
-  bucket = aws_s3_bucket.data_lake.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "log/"
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "data_lake_encryption" {
   bucket = aws_s3_bucket.data_lake.id
 
@@ -52,17 +20,6 @@ resource "random_string" "suffix" {
   length  = 8
   special = false
   upper   = false
-}
-
-
-# Block Public Access
-resource "aws_s3_bucket_public_access_block" "data_lake_logs_block_public_access" {
-  bucket = aws_s3_bucket.log_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
 
 # Block Public Access
@@ -84,11 +41,52 @@ resource "aws_s3_bucket_versioning" "data_lake_versioning" {
   }
 }
 
+# Logging
+resource "aws_s3_bucket" "data_lake_log" {
+  bucket = "${var.aws_s3_bucket_data_lake_name}-log-${random_string.suffix.result}"
+  tags = {
+    Name = "Log bucket"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket_encryption" {
+  bucket = aws_s3_bucket.data_lake_log.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = "arn"
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_acl" "data_lake_log_acl" {
+  bucket = aws_s3_bucket.data_lake_log.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_logging" "data_lake_logging" {
+  bucket = aws_s3_bucket.data_lake.id
+
+  target_bucket = aws_s3_bucket.data_lake_log.id
+  target_prefix = "log/"
+}
+
 #Enable versioning
 resource "aws_s3_bucket_versioning" "data_lake_log_versioning" {
-  bucket = aws_s3_bucket.log_bucket.id
+  bucket = aws_s3_bucket.data_lake_log.id
 
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+# Block Public Access
+resource "aws_s3_bucket_public_access_block" "data_lake_logs_block_public_access" {
+  bucket = aws_s3_bucket.data_lake_log.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
